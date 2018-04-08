86609d7f560c57b1dddde2413378119466d14dac
aQaTL <mmsoltys@outlook.com>
2018-04-08 18:43:41 +0200

    Loading file to splitted by line ending string

--- fileutils.go
+++ fileutils.go
@@ -1,6 +1,9 @@
 package fileutils
 
-import "io/ioutil"
+import (
+	"io/ioutil"
+	"strings"
+)
 
 func MustLoadToString(filename string) string {
 	str, err := LoadToString(filename)
@@ -13,4 +16,28 @@ func MustLoadToString(filename string) string {
 func LoadToString(filename string) (string, error) {
 	bytes, err := ioutil.ReadFile(filename)
 	return string(bytes), err
+}
+
+func MustLoadLines(filename string, windowsLineEnding bool) []string {
+	lines, err := LoadLines(filename, windowsLineEnding)
+	if err != nil {
+		panic(err)
+	}
+	return lines
+}
+
+func LoadLines(filename string, windowsLineEnding bool) ([]string, error) {
+	var lineSeparator string
+	if windowsLineEnding {
+		lineSeparator = "\r\n"
+	} else {
+		lineSeparator = "\n"
+	}
+
+	str, err := LoadToString(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	return strings.Split(str, lineSeparator), nil
 }
